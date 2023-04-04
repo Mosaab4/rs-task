e6932abf76bee4ba8285f2d524f7e20b2fbd442d
Mosaab <musab.ehab@gmail.com>
2023-04-04 15:21:33 +0200

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,11 +5,11 @@ cp .env.example .env
 echo "=== copy is done!  ==="
 
 echo "=== Execute docker-compose build... ==="
-./vendor/bin/sail up -d
+docker-compose build
 echo "=== docker-compose build is done!! ==="
 
 echo "=== Installing composer... ==="
-./vendor/bin/sail composer install
+docker-compose run laravel.test composer update
 echo "=== Composer build is done!! ==="
 
 echo "==== Generating the app key ==="
@@ -19,3 +19,7 @@ echo "==== App key generating is done!! ==="
 echo "==== Migrating and seed database ==="
 ./vendor/bin/sail php artisan migrate:fresh --seed
 echo "==== Migrating and seed database is done!! ==="
+
+echo "==== Running service ==="
+./vendor/bin/sail up -d
+echo "==== Running service is done!! ==="
