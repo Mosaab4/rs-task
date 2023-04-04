250207d465fe95f10e76d6006adfd7aebb4e1ed9
Mosaab <musab.ehab@gmail.com>
2023-04-04 17:01:06 +0200

    update docker files

--- setup.sh
+++ setup.sh
@@ -5,15 +5,16 @@ cp .env.example .env
 echo "=== copy is done!  ==="
 
 echo "=== Execute docker-compose build... ==="
-docker-compose build
+docker-compose up -d --build
 echo "=== docker-compose build is done!! ==="
 
 echo "=== Installing composer... ==="
-docker-compose run app composer update
+docker-compose run app composer install
 echo "=== Composer build is done!! ==="
 
 echo "==== Generating the app key ==="
 docker-compose run app php artisan key:generate
+docker-compose run app php artisan config:clear
 echo "==== App key generating is done!! ==="
 
 echo "==== Migrating and seed database ==="
