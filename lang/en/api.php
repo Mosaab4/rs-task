5b7d7fa4b06f06da236fabbc4a33b1f8bee29a88
Mosaab <musab.ehab@gmail.com>
2023-04-04 09:29:22 +0200

    add lang files

---
+++ lang/en/api.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'completed_trip' => 'The trip is completed, can not reserve seats',
+    'seats_more_than_available' => 'Selected Seats Exceed Available Seats',
+    'exceeded_available_seats' => 'can not reserve these seats, :count seats only available',
+    'not_available_seats' => 'This seats are already reserved: :seats please select another seats',
+    'can_not_reserve_from_this_station' => 'You can not reserve a trip from this station',
+];
